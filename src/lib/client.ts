9193d66e520821cbeaae6639a40ad1fa888ac5f7
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-06-13 13:43:46 +0200

    perf: use internal DNS for API fetching

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,7 +1,9 @@
 import Axios from 'axios';
 
 const client = Axios.create({
-  baseURL: 'https://api.egdata.app',
+  baseURL: import.meta.env.SSR
+    ? process.env.SERVER_API_ENDPOINT ?? 'https://api.egdata.app'
+    : 'https://api.egdata.app',
   headers: {
     'Content-Type': 'application/json',
   },
