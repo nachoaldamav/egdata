b61672c3ce4d478c4aad14257beebf53086ed028
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-06-19 02:59:55 +0200

    fix: API endpoint

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,10 +1,9 @@
 import Axios from 'axios';
 
 const client = Axios.create({
-  // baseURL: import.meta.env.SSR
-  //   ? process.env.SERVER_API_ENDPOINT ?? 'https://api.egdata.app'
-  //   : 'https://api.egdata.app',
-  baseURL: 'http://localhost:4000',
+  baseURL: import.meta.env.SSR
+    ? process.env.SERVER_API_ENDPOINT ?? 'https://api.egdata.app'
+    : 'https://api.egdata.app',
   headers: {
     'Content-Type': 'application/json',
   },
