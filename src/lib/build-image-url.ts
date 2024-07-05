2060467a38683400ff919c64aac713432cfee675
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-07-05 15:00:23 +0200

    fix: use `w` for image rescaling

--- src/lib/build-image-url.ts
+++ src/lib/build-image-url.ts
@@ -1,4 +1,4 @@
 export type ImageQuality = 'original' | 'low' | 'medium' | 'high';
 
 export default (src: string, height: number, quality: ImageQuality = 'medium') =>
-  `${src}?h=${height}&quality=${quality}&resize=1`;
+  `${src}?w=${height}&quality=${quality}&resize=1`;
