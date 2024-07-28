a0e885dd86c37100f7a74060ad5dc6ac2cbe9270
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-07-28 23:19:49 +0200

    feat: icons

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -56,6 +56,31 @@ export const meta: MetaFunction = () => {
       rel: 'canonical',
       href: 'https://egdata.app',
     },
+    {
+      tagName: 'link',
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      tagName: 'link',
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
+    },
+    {
+      tagName: 'link',
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png',
+    },
+    {
+      tagName: 'link',
+      rel: 'manifest',
+      href: '/site.webmanifest',
+    },
   ];
 };
 
