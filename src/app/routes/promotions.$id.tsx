b0769dac27300c990f8d91f726802faad73d53e3
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-07-30 01:26:11 +0200

    feat: promotions page improvements

--- src/app/routes/promotions.$id.tsx
+++ src/app/routes/promotions.$id.tsx
@@ -1,20 +1,33 @@
 import { useLoaderData, type MetaFunction } from '@remix-run/react';
-import { client } from '~/lib/client';
+import { client, getQueryClient } from '~/lib/client';
 import { getImage } from '~/lib/getImage';
 import { Image } from '~/components/app/image';
 import type { SingleOffer } from '~/types/single-offer';
-import { GameCard, OfferCard } from '~/components/app/offer-card';
-import { OfferListItem } from '~/components/app/game-card';
+import { OfferCard } from '~/components/app/offer-card';
 import { type LoaderFunctionArgs, redirect } from '@remix-run/node';
 import getPagingPage from '~/lib/get-paging-page';
 import getCountryCode from '~/lib/get-country-code';
 import { checkCountryCode } from '~/lib/check-country';
 import cookie from 'cookie';
-import { useState } from 'react';
 import { Button } from '~/components/ui/button';
-import { ReloadIcon } from '@radix-ui/react-icons';
 import { useCountry } from '~/hooks/use-country';
 import { usePreferences } from '~/hooks/use-preferences';
+import {
+  dehydrate,
+  HydrationBoundary,
+  keepPreviousData,
+  useInfiniteQuery,
+} from '@tanstack/react-query';
+import { OfferListItem } from '~/components/app/game-card';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '~/components/ui/select';
+import { ArrowDownIcon, GridIcon, ListBulletIcon } from '@radix-ui/react-icons';
+import { useState } from 'react';
 import { cn } from '~/lib/utils';
 
 export const meta: MetaFunction<typeof loader> = ({ params, data }) => {
@@ -136,11 +149,45 @@ export const meta: MetaFunction<typeof loader> = ({ params, data }) => {
   }
 };
 
+const fetchPromotionData = async ({
+  id,
+  country,
+  page,
+  sortBy,
+  sortDir,
+}: {
+  id: string;
+  country: string;
+  page: number;
+  sortBy: SortBy | null;
+  sortDir: 'asc' | 'desc' | null;
+}) => {
+  const { data } = await client.get<{
+    elements: SingleOffer[];
+    title: string;
+    start: number;
+    page: number;
+    count: number;
+  }>(`/promotions/${id}`, {
+    params: {
+      country,
+      page,
+      limit: 20,
+      sortBy: sortBy || undefined,
+      sortDir: sortDir || undefined,
+    },
+  });
+  return data;
+};
+
 export async function loader({ params, request }: LoaderFunctionArgs) {
   const { id } = params;
+  const queryClient = getQueryClient();
   const url = new URL(request.url);
   const page = getPagingPage(url);
   const country = getCountryCode(url, cookie.parse(request.headers.get('Cookie') || ''));
+  const sortBy = (url.searchParams.get('sortBy') ?? 'lastModifiedDate') as SortBy | null;
+  const sortDir = (url.searchParams.get('sortDir') ?? 'desc') as 'asc' | 'desc' | null;
 
   // Check if the country is a valid ISO code using Intl API
   if (!checkCountryCode(country)) {
@@ -148,86 +195,137 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
     return redirect(`/promotions/${id}?country=US&limit=20&page=1`, 302);
   }
 
-  const [promotionData, coverData] = await Promise.allSettled([
-    client.get<{
-      elements: SingleOffer[];
-      title: string;
-      limit: number;
-      start: number;
-      page: number;
-      count: number;
-    }>(`/promotions/${id}?country=${country}&page=${page}&limit=20`),
-    client.get(`/promotions/${id}/cover`),
+  if (!id) {
+    return redirect('/');
+  }
+
+  const [coverData, initialData] = await Promise.allSettled([
+    queryClient.fetchQuery({
+      queryKey: ['promotion-cover', { id }],
+      queryFn: () =>
+        client
+          .get<Pick<SingleOffer, '_id' | 'id' | 'namespace' | 'title' | 'keyImages'>>(
+            `/promotions/${id}/cover`,
+          )
+          .then((res) => res.data),
+    }),
+    queryClient.fetchQuery({
+      queryKey: ['promotion-meta', { id, country, limit: 20, sortBy, sortDir, page: 1 }],
+      queryFn: () => fetchPromotionData({ id, country, page: 1, sortBy, sortDir }),
+    }),
+    queryClient.prefetchInfiniteQuery({
+      queryKey: ['promotion', { id, country, sortBy, sortDir, limit: 20 }],
+      queryFn: ({ pageParam }) =>
+        fetchPromotionData({
+          id,
+          country,
+          page: pageParam as number,
+          sortBy,
+          sortDir,
+        }),
+      initialPageParam: page,
+      getNextPageParam: (
+        lastPage: { elements: SingleOffer[]; start: number; count: number },
+        allPages: { elements: SingleOffer[]; start: number; count: number }[],
+      ) => {
+        // If the start is greater than the count, we have reached the end
+        if (lastPage.start + 20 > lastPage.count) {
+          return undefined;
+        }
+
+        return allPages?.length + 1;
+      },
+    }),
   ]);
 
-  const promotion = promotionData.status === 'fulfilled' ? promotionData.value.data : null;
-  const cover = coverData.status === 'fulfilled' ? coverData.value.data : null;
+  const cover = coverData.status === 'fulfilled' ? coverData.value : null;
 
   return {
-    promotion,
     cover,
     id,
+    promotion: initialData.status === 'fulfilled' ? initialData.value : null,
+    dehydratedState: dehydrate(queryClient),
   };
 }
 
-export default function Promotion() {
+export default function Index() {
+  const { dehydratedState } = useLoaderData<typeof loader>();
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <Promotion />
+    </HydrationBoundary>
+  );
+}
+
+type SortBy =
+  | 'releaseDate'
+  | 'lastModifiedDate'
+  | 'effectiveDate'
+  | 'creationDate'
+  | 'viewableDate'
+  | 'pcReleaseDate'
+  | 'upcoming'
+  | 'price';
+
+const sortByDisplay: Record<SortBy, string> = {
+  releaseDate: 'Release Date',
+  lastModifiedDate: 'Modified Date',
+  effectiveDate: 'Effective Date',
+  creationDate: 'Creation Date',
+  viewableDate: 'Viewable Date',
+  pcReleaseDate: 'PC Release Date',
+  upcoming: 'Upcoming',
+  price: 'Price',
+};
+
+function Promotion() {
   const { country } = useCountry();
-  const { view } = usePreferences();
-  const { cover, promotion: promotionInitialData, id } = useLoaderData<typeof loader>();
-  const [promotion, setPromotion] = useState<{
-    elements: SingleOffer[];
-    title: string;
-    limit: number;
-    start: number;
-    page: number;
-    count: number;
-  } | null>(promotionInitialData);
-  const [loading, setLoading] = useState(false);
+  const { view, setView } = usePreferences();
+  const [sortBy, setSortBy] = useState<SortBy>('lastModifiedDate');
+  const [sortDir, setSortDir] = useState<'asc' | 'desc'>('desc');
+  const { cover, id } = useLoaderData<typeof loader>();
+  const {
+    data: promotion,
+    isLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isFetching,
+  } = useInfiniteQuery({
+    queryKey: ['promotion', { id, country, sortBy, sortDir, limit: 20 }],
+    queryFn: ({ pageParam }) =>
+      fetchPromotionData({
+        id,
+        country,
+        page: pageParam as number,
+        sortBy,
+        sortDir,
+      }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.start + 20 > lastPage.count) {
+        return undefined;
+      }
+
+      return allPages?.length + 1;
+    },
+    placeholderData: keepPreviousData,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   if (!promotion) {
     return null;
   }
 
-  const handleLoadMore = async () => {
-    setLoading(true);
-    const { page } = promotion;
-    const newPage = page + 1;
-    const { data } = await client
-      .get<{
-        elements: SingleOffer[];
-        page: number;
-        total: number;
-        limit: number;
-      }>(`/promotions/${id}?country=${country}&page=${newPage}&limit=20`)
-      .catch(() => ({
-        data: {
-          elements: [],
-          page: 0,
-          total: 0,
-          limit: 0,
-        },
-      }));
-
-    if (data.elements.length) {
-      // @ts-expect-error
-      setPromotion((prev) => ({
-        ...prev,
-        // @ts-expect-error
-        elements: [...prev.elements, ...data.elements],
-        page: newPage,
-        limit: data.limit,
-      }));
-    }
-
-    setLoading(false);
-  };
-
   return (
     <main className="container mx-auto">
       <div className="relative h-96 overflow-hidden rounded-2xl">
         <div className="absolute inset-0 flex flex-col justify-center items-start text-white p-8 bg-gradient-to-r from-black/80 to-black/30 z-10">
-          <h1 className="text-5xl font-bold">{promotion?.title}</h1>
-          <p className="mt-4 text-lg">{promotion?.count} offers available in this event</p>
+          <h1 className="text-5xl font-bold">{promotion.pages[0].title}</h1>
+          <p className="mt-4 text-lg">{promotion.pages[0]?.count} offers available in this event</p>
         </div>
         {cover && (
           <Image
@@ -239,33 +337,20 @@ export default function Promotion() {
                 'DieselStoreFrontWide',
               ]).url
             }
-            alt={promotion?.title}
+            alt={promotion.pages[0].title}
             width={1920}
             height={1080}
             className="w-full h-full object-cover"
           />
         )}
       </div>
-      <section
-        className={cn(
-          'mt-8 grid gap-4',
-          view === 'grid' ? 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-5' : 'grid-cols-1',
-        )}
-      >
-        {promotion.elements.map((game) => {
-          if (view === 'list') {
-            return <OfferListItem key={game.id} game={game} />;
-          }
-
-          return <OfferCard offer={game} key={game.id} size="md" />;
-        })}
-      </section>
-      <div className="flex justify-center mt-8">
-        <Button
-          disabled={loading || promotion.limit * promotion.page >= promotion.count}
-          onClick={handleLoadMore}
-        >
-          {loading && (
+      <header className="flex flex-row justify-between items-center gap-4 mt-5">
+        <div className="inline-flex items-center gap-2">
+          <h2 className="text-2xl">Results</h2>
+          <span className="text-sm text-gray-500">
+            ({promotion.pages.reduce((acc, page) => acc + page.elements.length, 0)} results)
+          </span>
+          {isFetching && (
             <svg
               className="animate-spin -ml-1 mr-3 h-5 w-5"
               xmlns="http://www.w3.org/2000/svg"
@@ -279,12 +364,92 @@ export default function Promotion() {
                 r="10"
                 stroke="currentColor"
                 stroke-width="4"
-              ></circle>
+              />
               <path
                 className="opacity-75"
                 fill="currentColor"
                 d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
+              />
+            </svg>
+          )}
+        </div>
+        <div className="flex flex-row gap-2">
+          <Select value={sortBy} onValueChange={(value) => setSortBy(value as SortBy)}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue className="text-sm">{sortByDisplay[sortBy]}</SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+              {Object.entries(sortByDisplay).map(([key, value]) => (
+                <SelectItem key={key} value={key}>
+                  {value}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <Button
+            variant="outline"
+            className="h-9 w-9 p-0"
+            onClick={() => setSortDir(sortDir === 'asc' ? 'desc' : 'asc')}
+          >
+            <ArrowDownIcon
+              className={cn(
+                'h-5 w-5 transform transition-transform',
+                sortDir === 'asc' ? '-rotate-180' : 'rotate-0',
+              )}
+              aria-hidden="true"
+            />
+          </Button>
+          <Button
+            variant="outline"
+            className="h-9 w-9 p-0"
+            onClick={() => setView(view === 'grid' ? 'list' : 'grid')}
+          >
+            {view === 'grid' ? (
+              <ListBulletIcon className="h-5 w-5" aria-hidden="true" />
+            ) : (
+              <GridIcon className="h-5 w-5" aria-hidden="true" />
+            )}
+          </Button>
+        </div>
+      </header>
+      <div
+        className={cn(
+          'mt-8 grid gap-4',
+          view === 'grid' ? 'grid-cols-2 lg:grid-cols-3 xl:grid-cols-5' : 'grid-cols-1',
+        )}
+      >
+        {promotion.pages
+          .flatMap((page) => page.elements)
+          .map((game) =>
+            view === 'grid' ? (
+              <OfferCard offer={game} key={game.id} size="md" />
+            ) : (
+              <OfferListItem game={game} key={game.id} />
+            ),
+          )}
+      </div>
+      <div className="flex justify-center mt-8">
+        <Button disabled={!hasNextPage} onClick={() => fetchNextPage()}>
+          {isFetchingNextPage && (
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              />
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              />
             </svg>
           )}
           Load More
