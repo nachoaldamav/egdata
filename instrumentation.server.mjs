acb746c275a3b78fe84e7c2bb0ad484daee30717
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-08-14 19:04:20 +0200

    chore: remove sentry

--- instrumentation.server.mjs
+++
@@ -1,7 +0,0 @@
-import * as Sentry from "@sentry/remix";
-
-Sentry.init({
-    dsn: "https://6ca5796e7f66a21fc7c0299e5b273c8a@o4506558481498112.ingest.us.sentry.io/4507667527565312",
-    tracesSampleRate: 1,
-    autoInstrumentRemix: true
-})
