89fda907506ab3e350da61bf74493e5575c1dffd
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-11-10 18:39:58 +0100

    fix: logout user

--- app/routes/auth/logout.tsx
+++ app/routes/auth/logout.tsx
@@ -1,19 +1,15 @@
-import { deleteCookie } from '@/lib/cookies';
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/auth/logout')({
   component: () => <div>Hello /auth/logout!</div>,
 
   beforeLoad: async () => {
-    if (import.meta.env.SSR) {
-      const { deleteCookie } = await import('vinxi/http');
-      deleteCookie('EGDATA_AUTH');
-    } else {
-      await deleteCookie('EGDATA_AUTH');
-    }
-
     throw redirect({
       to: '/',
+      headers: {
+        'Set-Cookie':
+          'EGDATA_AUTH=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT',
+      },
     });
   },
 });
