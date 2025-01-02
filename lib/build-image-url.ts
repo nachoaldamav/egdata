6aa74ec42f304c20ede11ad1d8e1b73548fba5e2
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2025-01-02 13:41:00 +0100

    chore: revert CDN image optimizations

--- lib/build-image-url.ts
+++ lib/build-image-url.ts
@@ -1,21 +1,7 @@
 export type ImageQuality = 'original' | 'low' | 'medium' | 'high';
 
-const qualityMap: Record<ImageQuality, number> = {
-  original: 100,
-  high: 80,
-  medium: 60,
-  low: 40,
-};
-
 export default (
   src: string,
   height: number,
   quality: ImageQuality = 'medium',
-) => {
-  const hasExtension = src.includes('.');
-  if (!hasExtension || src[0] === '/') {
-    return src;
-  }
-
-  return `https://cdn.egdata.app/cdn-cgi/image/height=${height},quality=${qualityMap[quality]}/${src}`;
-};
+) => `${src}?w=${height}&quality=${quality}&resize=1`;
