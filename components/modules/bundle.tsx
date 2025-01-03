fb651b69a79264cfa363bf678fa3737fcb578578
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2025-01-03 12:49:29 +0100

    fix: set `ShowNavigation` to false

--- components/modules/bundle.tsx
+++ components/modules/bundle.tsx
@@ -22,6 +22,7 @@ import { EqualIcon } from 'lucide-react';
 import { EGSIcon } from '../icons/egs';
 import { Link } from '@tanstack/react-router';
 import { useLocale } from '@/hooks/use-locale';
+import { getBuyLink } from '@/lib/get-build-link';
 
 const trackEvent = (
   offers: { id: string; namespace: string }[],
@@ -248,35 +249,4 @@ export function Bundle({ id, offer }: { id: string; offer: SingleOffer }) {
       </div>
     </div>
   );
-}
-
-// Bundle direct link
-// https://store.epicgames.com/purchase?highlightColor=0078f2&offers=1-883178bfb8b94ce9b95e1e60bec225ed-48a399d8a7f6448daf7bdee752c8fb6d-&showNavigation=true
-
-// Multioffer direct link
-// https://store.epicgames.com/purchase?highlightColor=0078f2&offers=1-42c70202dc684966a827bfcb7b49ac5f-cd1610921c934f588d95b7d0d3750730-&offers=1-dbfff2d3f4dc446b8f4931ed8a353685-90d3d1bf246c4aeda03e36a167b463a2-&offers=1-e424e1d51e114ae8b6d685c943043cd7-f8b1645ba22a44439f3b68daec1ba1ee-&showNavigation=true
-
-function getBuyLink({ offers }: { offers: SingleOffer[] }): string {
-  const offerList: {
-    offerId: string;
-    namespace: string;
-    quantity: number;
-  }[] = offers.map((offer) => ({
-    offerId: offer.id,
-    namespace: offer.namespace,
-    quantity: 1,
-  }));
-
-  const url = new URL('https://store.epicgames.com/purchase');
-  url.searchParams.set('showNavigation', 'true');
-  url.searchParams.set('highlightColor', '4ade80');
-
-  for (const offer of offerList) {
-    url.searchParams.append(
-      'offers',
-      `${offer.quantity}-${offer.namespace}-${offer.offerId}-`,
-    );
-  }
-
-  return url.toString();
 }
