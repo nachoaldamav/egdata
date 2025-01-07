ab995a0cf20e0cb5888108f3dfb1d52054b22a6d
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2025-01-07 18:18:00 +0100

    fix: add `_build` to excluded routes

--- patch-build.js
+++ patch-build.js
@@ -15,4 +15,10 @@ const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'));
 
 manifest.exclude.push('/_build/*');
 
+// Remove all the exlcuded routes that start with /assets and replace them with /assets/*
+manifest.routes = manifest.routes.filter(
+  (route) => !route.startsWith('/assets'),
+);
+manifest.routes.push('/assets/*');
+
 fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
